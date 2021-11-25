eddb781dc2d9523420851d93160424679a6716ca
IGOR CRUZ <igorcruz.it@outlook.com>
2021-11-25 16:45:19 -0300

    editando o button

--- pages/contato.js
+++ pages/contato.js
@@ -6,7 +6,7 @@ const Contato = () => {
   return (
     <div className='text-center m-12'>
       <PageTitle title='Contato' />
-      <a className='bold' href={"https://api.whatsapp.com/send?phone=71982889448&text=Oi gostaria de  realizar uma reserva"} target='blank'>  Whatsapp</a>
+      <a className='bold mb-6 text-center bg-blue-100 border-t border-b border-gren-500 text-green-700 px-4 py-3' href={"https://api.whatsapp.com/send?phone=71982889448&text=Oi gostaria de  realizar uma reserva"} target='blank'>  Whatsapp</a>
       <br />
 
 
