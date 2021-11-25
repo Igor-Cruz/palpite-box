fc28f9ec3416bb23ce1c8a4ce75ff7c83559caeb
IGOR CRUZ <igorcruz.it@outlook.com>
2021-11-25 16:24:40 -0300

    Adicionando o contato

--- pages/contato.js
+++ pages/contato.js
@@ -6,7 +6,7 @@ const Contato = () => {
   return (
     <div className='text-center m-12'>
       <PageTitle title='Contato' />
-      <a className='bold' href={"https://api.whatsapp.com/send?phone=71982889448&text=Oi gostaria de realizar uma reserva"} target='blank'>  Whatsapp</a>
+      <a className='bold' href={"https://api.whatsapp.com/send?phone=71982889448&text=Oi gostaria de  realizar uma reserva"} target='blank'>  Whatsapp</a>
       <br />
 
 
