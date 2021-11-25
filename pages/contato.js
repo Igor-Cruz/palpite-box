2645606e87e81229f5bbd445acfe0d6d8c5a20ff
IGOR CRUZ <igorcruz.it@outlook.com>
2021-11-25 16:05:35 -0300

    adicionando o Whatsapp

--- pages/contato.js
+++ pages/contato.js
@@ -4,15 +4,14 @@ import PageTitle from '../components/PageTitle'
 
 const Contato = () => {
   return (
-    <div>
+    <div className='text-center m-12'>
       <PageTitle title='Contato' />
-      <h1>Contato</h1>
-      <div>
-        <Link href='/'>
-          <a>Home</a>
-        </Link>
-      </div>
+      <a className='bold' href={"https://api.whatsapp.com/send?phone=71982889448&text=Oi gostaria de realizar uma reserva"} target='blank'>  Whatsapp</a>
+      <br />
+
+
     </div>
+
   )
 }
 
